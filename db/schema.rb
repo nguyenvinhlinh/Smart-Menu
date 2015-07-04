d519a5b8e81deb61d10ad7ace4a70785af45b7a1
nguyenvinhlinh <nguyenvinhlinh93@gmail.com>
2015-07-05 01:24:04 +0700

    DB::Add new image column to MenuItem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704074222) do
+ActiveRecord::Schema.define(version: 20150704182138) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -30,5 +30,7 @@ ActiveRecord::Schema.define(version: 20150704074222) do
     t.string   "taste"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end  
+    t.text     "image"
+  end
+
 end
